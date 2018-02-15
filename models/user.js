760f1b339c58d27f40ef606ba4c8323f73fb0a4f
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-02-15 21:07:53 +0100

    fixed user model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,29 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+	var User = sequelize.define(
+		'User',
+		{
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: DataTypes.STRING
+			},
+			username: DataTypes.STRING,
+			full_name: DataTypes.STRING,
+			email: {
+				type: DataTypes.STRING,
+				unique: true
+			},
+			token: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true
+			}
+		},
+		{}
+	);
+	User.associate = function(models) {
+		// associations can be defined here
+	};
+	return User;
 };
