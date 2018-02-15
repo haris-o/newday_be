56b486d19cd4d8062e8664dff8c67100ecf2ac31
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-02-15 21:19:51 +0100

    configured user migration

--- migrations/20180215194537-create-user.js
+++ migrations/20180215194537-create-user.js
@@ -1,30 +1,38 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('Users', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.STRING
+			},
+			username: {
+				type: Sequelize.STRING
+			},
+			full_name: {
+				type: Sequelize.STRING
+			},
+			email: {
+				type: Sequelize.STRING,
+				unique: true
+			},
+			token: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				unique: true
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('Users');
+	}
 };
--- models/userrole.js
+++ models/userrole.js
@@ -1,10 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var UserRole = sequelize.define('UserRole', {
-    name: DataTypes.STRING
-  }, {});
-  UserRole.associate = function(models) {
-    // associations can be defined here
-  };
-  return UserRole;
+	var UserRole = sequelize.define(
+		'UserRole',
+		{
+			name: DataTypes.STRING
+		},
+		{}
+	);
+	UserRole.associate = function(models) {
+		// associations can be defined here
+	};
+	return UserRole;
 };
