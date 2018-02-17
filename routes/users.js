c7a9a3a04cfcd9fdcbc46cd4accf77ee0dbd33dd
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-02-17 12:06:44 +0100

    added user crud skeleton

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+	//get all users
+});
+
+router.post('/', (req, res, next) => {
+	//add a new user
+});
+
+router.put('/:id', (req, res, next) => {
+	//edit a user
+});
+
+router.delete('/:id', (req, res, next) => {
+	//delete a user
 });
 
 module.exports = router;
