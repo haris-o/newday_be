16a475c88323b0789119650769d39b2ef864a2dd
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-02-17 13:41:20 +0100

    added nodemon, began fixing get /

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
 	models.User.findAll()
 		.then(users =>
 			res.status(200).json({
@@ -17,15 +17,27 @@ router.get('/', (req, res, next) => {
 		);
 });
 
-router.post('/', (req, res, next) => {
-	//add a new user
+router.post('/', (req, res) => {
+	const attributes = req.body;
+	models.User.create(attributes)
+		.then(user =>
+			res.status(201).json({
+				message: 'User created.',
+				user: user
+			})
+		)
+		.catch(error => {
+			res.status(400).json({
+				error: error
+			});
+		});
 });
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', (req, res) => {
 	//edit a user
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', (req, res) => {
 	//delete a user
 });
 
