b6555bde0b7a49ef1aa1d977e52dac8e8cbd7a77
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-02-18 18:32:30 +0100

    done for the day

--- routes/users.js
+++ routes/users.js
@@ -19,24 +19,10 @@ router.get('/', (req, res) => {
 		);
 });
 
-router.post('/', (req, res) => {
-	const attributes = req.body;
-	models.User.create(attributes)
-		.then(user =>
-			res.status(201).json({
-				message: 'User created.',
-				data: user
-			})
-		)
-		.catch(error => {
-			res.status(400).json({
-				error: error
-			});
-		});
-});
-
 router.put('/:id', (req, res) => {
-	//edit a user
+	const userId = req.params.id;
+
+	//models.User.
 });
 
 router.delete('/:id', (req, res) => {
