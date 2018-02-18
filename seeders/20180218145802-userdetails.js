f6ea133b04d63083f373702ddd16ee8b27ce595e
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-02-18 17:49:08 +0100

    seeds done

--- seeders/20180218145802-userdetails.js
+++ seeders/20180218145802-userdetails.js
@@ -5,15 +5,21 @@ var faker = require('faker');
 module.exports = {
 	up: (queryInterface, Sequelize) => {
 		let userDetails = [];
-		//for (let i = 10; i < 15; i++) {
-		userDetails.push({
-			UserId: '14',
-			createdAt: '2018-02-18T08:09:33.027Z',
-			updatedAt: '2018-02-18T15:44:49.004Z'
-		});
-
-		console.log(userDetails[userDetails.length - 1]);
-		//}
+		for (let i = 0; i < 100; i++) {
+			let rand = Math.random();
+			userDetails.push({
+				UserId: i,
+				firstName: rand < 0.7 ? faker.name.firstName() : null,
+				lastName: rand < 0.7 ? faker.name.lastName() : null,
+				username: rand < 0.4 ? faker.internet.userName() : null,
+				dateOfBirth: rand > 0.2 ? faker.date.past(20) : null,
+				weight: rand < 0.8 ? faker.random.number({ min: 40, max: 160 }) : null,
+				height: rand < 0.8 ? faker.random.number({ min: 140, max: 210 }) : null,
+				isFemale: rand < 0.7,
+				createdAt: '2018-02-18T08:09:33.027Z',
+				updatedAt: '2018-02-18T15:44:49.004Z'
+			});
+		}
 
 		return queryInterface.bulkInsert('UserDetails', userDetails, {});
 	},
