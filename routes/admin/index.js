a0038e9831d119dd1973251ee42094a9de3a358e
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-03-19 23:35:40 +0100

    removed comments from index.js

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/***********
- * Importing all admin routers
- ***********/
-
 var users = require('./users');
-
-/***********
- * Creating API checking mechanism
- ***********/
 
 router.use(function(req, res, next) {
 	if (req.token.UserRoleId !== 0) {
