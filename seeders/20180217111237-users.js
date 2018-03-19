2ac6782f2ea1bb3203cba63845a9284cb320ea70
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-03-19 18:58:23 +0100

    seeders configured

--- seeders/20180217111237-users.js
+++ seeders/20180217111237-users.js
@@ -7,30 +7,40 @@ module.exports = {
 	up: (queryInterface, Sequelize) => {
 		let users = [
 			{
+				id: faker.random.uuid(),
 				provider: 'local',
 				email: 'user@user.com',
 				password: bcrypt.hashSync('user', 10),
-				UserRoleId: 1
+				UserRoleId: 1,
+				createdAt: new Date(),
+				updatedAt: new Date()
 			},
 			{
+				id: faker.random.uuid(),
 				provider: 'local',
 				email: 'admin@admin.com',
 				password: bcrypt.hashSync('admin', 10),
-				UserRoleId: 0
+				UserRoleId: 0,
+				createdAt: new Date(),
+				updatedAt: new Date()
 			},
 			{
 				id: '75e47589-d8b9-49a0-8394-8359e6471f11',
 				provider: 'google',
 				providerId: '100904279359627792666',
 				email: faker.internet.email(),
-				UserRoleId: 1
+				UserRoleId: 1,
+				createdAt: new Date(),
+				updatedAt: new Date()
 			},
 			{
 				id: '854f96ca-63b7-4b42-8194-e15500007887',
 				provider: 'facebook',
 				providerId: '1721207781250672',
 				email: faker.internet.email(),
-				UserRoleId: 1
+				UserRoleId: 1,
+				createdAt: new Date(),
+				updatedAt: new Date()
 			}
 		];
 
