0e277f45a4e1a6f14ee17075ea7706668af64bf5
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2018-04-08 22:09:50 +0200

    fixed bugs with validator.js, all inputs are now converted to strings first

--- routes/api/details/validation.js
+++ routes/api/details/validation.js
@@ -12,31 +12,31 @@ const validate = (req, res, next) => {
 			isFemale
 		} = req.body;
 
-		if (firstName && !validator.isAlpha(firstName)) {
+		if (firstName && !validator.isAlpha(firstName + '')) {
 			throw new Error('First name can only contain letters.');
 		}
 
-		if (lastName && !validator.isAlpha(lastName)) {
+		if (lastName && !validator.isAlpha(lastName + '')) {
 			throw new Error('Last name can only contain letters.')
 		}
 
-		if (nickname && !validator.isAscii(nickname)) {
+		if (nickname && !validator.isAscii(nickname + '')) {
 			throw new Error('Nickname can only contain letters and numbers.');
 		}
 
-		if (dateOfBirth && !validator.toDate(dateOfBirth)) {
+		if (dateOfBirth && !validator.toDate(dateOfBirth + '')) {
 			throw new Error('Date of birth must be a date.');
 		}
 
-		if (weight && !validator.isDecimal(weight)) {
+		if (weight && !validator.isDecimal(weight + '')) {
 			throw new Error('Weight must be a number.');
 		}
 
-		if (height && !validator.isDecimal(height)) {
+		if (height && !validator.isDecimal(height + '')) {
 			throw new Error('Height must be a number.');
 		}
 
-		if (isFemale !== undefined && !validator.isBoolean(isFemale)) {
+		if (isFemale !== undefined && !validator.isBoolean(isFemale + '')) {
 			throw new Error('Gender can only be female or not female.');
 		}
 
